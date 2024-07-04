326a1018308fd70f26e552724cc9813c352fb539
VVS Basanth Pedapati <vvs.pedapati@hotmail.com>
2024-07-04 20:37:34 +0530

    Gaac-Hub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>GAAC-hub</h1>
+      <h1>GAAC-Hub</h1>
       <p>A hub for GITAM Aero Astro Club members</p>
     </main>
   );
